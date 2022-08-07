49b499670607dd9a55b66c29b1cd5af17d37144a
abdulazisarifin03 <109236603+abdulazisarifin03@users.noreply.github.com>
2022-08-07 08:05:45 +0700

    Add files via upload

---
+++ coding.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>MUHAMMAD RIZKI ARIPIN</title>
+<link rel="stylesheet" type="text/css" href="Coding.css">
+</head>
+<body>
+  <form action="oding.php" method="post">
+    <center>
+    <div class="fivesolidblue">
+    <label>
+   Masukan nama lengkap<br>
+   <input type="teks" name="nama" placeholder="masukan nama">
+   </label><br>
+    <label>
+      jenis kelamin:<br>
+      <select>
+      <option>laki-laki</option>
+      <option>perempuan</option>
+      </select>
+     </label><br>
+     <br>
+     <br>
+     SKILL<br>
+    <label>
+       <input type="checkbox" name="html">HTML<br>
+    </label>
+    <label>
+       <input type="checkbox" name="c++">C++<b>
+    </label>
+    <br>
+    <br>
+    <label>
+      <textarea name="pesan" placeholder="Pesan"></textarea>
+    </label><br>
+    <label>
+      <input type="submit" name="submit" value="kirim">
+      </div>
+      </center>
+    </label>
+  </form>
+  </body>
+</html
